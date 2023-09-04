1a139e7066a76d8b251a5ee2257bd0f3ca25476f
Dylan Seligmann <dylanseligmann@gmail.com>
2023-09-04 17:38:37 -0300

    React-Tailwind-v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Team from './Components/Team';
+import ContactUs from './Components/ContactUs'
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+        <Navbar />
+
+        <Routes>
+          
+          <Route path='/' element={<Home/>} />
+          <Route path='/team' element={<Team/>} />
+          <Route path='/contactUs' element={<ContactUs/>}/>
+
+        </Routes>
+
+        <Footer/>
+        
+      
     </div>
   );
 }
